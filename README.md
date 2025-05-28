e74aa27803666e1d4bad5924bb3de39bf2e1519d
pang1217 <kissme.pa.2001@gmail.com>
2025-05-28 19:24:14 +0700

    updates

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # pomo
+pomo is a simple Bash script that provides utilities to support the **Pomodoro Technique**, also known as the **Tomato Timer**. It helps you manage your focus sessions by timing work intervals.
+
+## How to Run the Script
+To start a Pomodoro session with the default time of **25 minutes**, run :
+``` bash
+./pomo.sh
+```
+
+To set a custom Pomodoro duration (in minutes), run :
+``` bash
+./pomo.sh <minutes>
+
--- pomo.sh
+++ pomo.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
 
+# define color code
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+ENDCOLOR="\033[0m"
+
+# set default to 25 minutes if no argument is passed
 if [ -z "$1" ] ; then
     let minutes=25
 else 
     let minutes=$1
 fi
-echo "Waiting ${minutes} minutes"
 
-let Timer=$minutes*60
+# start message 
+echo -e "${GREEN}Set Timer ${minutes} minutes${ENDCOLOR}"
+say -v Daniel "Set Timer ${minutes} minutes"
 say -v Daniel "Timer start!!"
-sleep $Timer
 
-echo "Timer is done!"
+# Main loop
+for i in $(seq 1 $minutes) 
+do 
+    sleep 60
+    let rem=$minutes-i
+    echo -ne "⏳ ${RED}${rem}${ENDCOLOR} ${GREEN}minutes remaining${ENDCOLOR}\\r"
+
+    if ((rem%5==0 || rem<3 && rem>0)) ; then
+        say -v Daniel "${rem} minutes left."
+    fi
+
+done
+echo ""
+
+echo -e "${GREEN}Timer is done!${ENDCOLOR}"
 say -v Daniel "Timer is done!"
 
